932479c6f8992982a412e0939559c4a047ea077e
m-ji97 <tv5035@naver.com>
2023-12-26 16:31:19 +0900

    if-문법

---
+++ src/com/javaex/ex03/Ex01.java
@@ -0,0 +1,27 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("점수를 입력하세요"); 
+		
+		int point = sc.nextInt();
+		
+		if (point >= 60) {
+			//참일때
+			System.out.println("합격입니다.");
+			
+		}
+		//거짓일때
+		
+		sc.close();
+		
+		System.out.print("프로그램 종료"); 
+	}
+
+}
